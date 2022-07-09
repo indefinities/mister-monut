855edad64cfe633a514b6a6c43ac25cc936e3aa5
Nat Hsu <47017402+indefinities@users.noreply.github.com>
2022-07-08 23:18:45 -0400

    Add files via upload
    Initial

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import monutHome from './sections/monut-home.vue'
+</script>
+
+<template>
+  <monut-home />
+</template>
+
+<style>
+#app {
+  font-family: Avenir, sans-serif;
+}
+</style>
