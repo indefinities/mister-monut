1f20ae7e0187d0ddeed8a6e9dc805381e6fe353d
Nat Hsu <47017402+indefinities@users.noreply.github.com>
2022-07-22 15:53:19 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vuetify from 'vite-plugin-vuetify'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	base: '/<REPO>/',
+	base: '/mister-monut/',
 	plugins: [
 		vue(),
 		vuetify({ autoImport: true }),
