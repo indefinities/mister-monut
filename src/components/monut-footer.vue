06bb27cd4c35ddc3a80b74750a81b31a41562a27
Natalie <natalie.d.hsu@gmail.com>
2024-01-23 09:41:28 -0500

    change footer email

--- src/components/monut-footer.vue
+++ src/components/monut-footer.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="monut-footer">
-    <a href="mailto:nick@justkdog.com">📧 nick@justkdog.com</a>
+    <a href="mailto:hello@mistermonut.com">📧 hello@mistermonut.com</a>
     <p>🏪 873 Main Street, Waltham, MA</p>
     <a href="tel:781-893-1809">☎ (781) 893 - 1809</a>
     <p><em>mister monut</em> © {{ getCurrentYear }}</p>
